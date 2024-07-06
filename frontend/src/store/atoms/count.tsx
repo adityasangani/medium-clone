72505dd45a74057d3f46a152647c7f6d37641dd3
aditya <adityasangani21@gmail.com>
2024-07-06 23:59:47 +0530

    added recoil-persist

--- frontend/src/store/atoms/count.tsx
+++ frontend/src/store/atoms/count.tsx
@@ -3,8 +3,12 @@ export interface UserType {
   email: string;
 }
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 export const userAtom = atom<UserType | null>({
   key: "userAtom",
   default: null,
+  effects_UNSTABLE: [persistAtom],
 });
